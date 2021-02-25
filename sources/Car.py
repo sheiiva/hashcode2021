cc51e26515dbec531bd9af6ba040bfc5427ac963
Corentin ROZET <corentin.rozet@epitech.eu>
2021-02-25 19:45:21 +0100

    [ADD] Car class

---
+++ sources/Car.py
@@ -0,0 +1,18 @@
+###########################################
+#                 HASH CODE               #
+###########################################
+#                                         #
+# Corentin COUTRET-ROZET and Hugo LACHKAR #
+#                                         #
+###########################################
+
+
+class Car():
+
+    """
+    Car class that allows car informations.
+    """
+
+    def __init__(self):
+        self._path = []
+        self._position = 0
