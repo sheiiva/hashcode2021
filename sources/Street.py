545d705b11f260cbf0e6e23142c9a9506e7612a0
Corentin ROZET <corentin.rozet@epitech.eu>
2021-02-25 20:11:25 +0100

    [ADD] street parsing

--- sources/Street.py
+++ sources/Street.py
@@ -13,8 +13,8 @@ class Street():
     Street class that allows street informations and computation.
     """
 
-    def __init__(self, d: int, b: int, e: int, name: str):
-        self._length = d #length in seconds
+    def __init__(self, b: int, e: int, name: str, l: int):
         self._begin = b
         self._end = e
         self._name = name
+        self._length = l #length in seconds
--- sources/Trafic.py
+++ sources/Trafic.py
@@ -7,6 +7,7 @@
 ###########################################
 
 from sources.Car import Car
+from sources.Street import Street
 
 
 class Trafic():
@@ -19,7 +20,7 @@ class Trafic():
         self._info = 0
         self._car = None
         self._traficLight = None
-        self._street = None
+        self._street = []
 
     def parse(self, filename: str) -> None:
 
@@ -32,12 +33,21 @@ class Trafic():
                 exit(84)
 
         def parseGlobalInfo(infos: str) -> dict:
-            elems = [elem for elem in infos.split()]
+            elems = [int(elem) for elem in infos.split()]
             return {"d": elems[0], "i": elems[1], "s": elems[2], "v": elems[3], "f": elems[4]}
 
+        def parseStreetInfo(nbStreets: int, infos: str) -> None:
+
+            for i in range(1, nbStreets+1):
+                streetInfos = [elem for elem in infos[i].split()]
+                self._street.append(Street(int(streetInfos[0]), int(streetInfos[1]),  streetInfos[2], int(streetInfos[3])))
+
+
         fileContent = getContent(filename)
-        # PARSE BELOW
+        # Parse general informations
         self._info = parseGlobalInfo(fileContent[0])
+        # Parse streets informations
+        parseStreetInfo(self._info["s"], fileContent)
 
 
     def run(self, args: list) -> None:
